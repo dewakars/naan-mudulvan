50ab284bce9c1d5367d0cd0381dd48b2362f85ee
Dewakar S <124648745+dewakars@users.noreply.github.com>
2023-03-29 16:19:55 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,32 @@
+function f1() {
+	document.getElementById("textarea1").style.fontWeight = "bold";
+}
+
+function f2() {
+	document.getElementById("textarea1").style.fontStyle = "italic";
+}
+
+function f3() {
+	document.getElementById("textarea1").style.textAlign = "left";
+}
+
+function f4() {
+	document.getElementById("textarea1").style.textAlign = "center";
+}
+
+function f5() {
+	document.getElementById("textarea1").style.textAlign = "right";
+}
+
+function f6() {
+	document.getElementById("textarea1").style.textTransform = "uppercase";
+}
+
+function f7() {
+	document.getElementById("textarea1").style.textTransform = "lowercase";
+}
+
+function f8() {
+	document.getElementById("textarea1").style.textTransform = "capitalize";
+}
+Footer
